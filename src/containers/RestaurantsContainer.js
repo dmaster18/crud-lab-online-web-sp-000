4948fa5ac8e2002e1b366bc4c2ed1437a7089b53
David Strauch <dmaster18@gmail.com>
2021-01-23 20:32:53 +0000

    Automatically backed up by Learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -3,14 +3,13 @@ import { connect } from 'react-redux'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
 
-
 class RestaurantsContainer extends Component {
   render() {
     return (
       <div>
         <RestaurantInput addRestaurant={this.props.addRestaurant}/>
-        <Restaurants
-          restaurants={this.props.restaurants}
+        <Restaurants  restaurants={this.props.restaurants}
+         
           deleteRestaurant={this.props.deleteRestaurant}
         />
       </div>
