4faa66261ec191e649945659cb5f1ee71114b3c9
David Strauch <dmaster18@gmail.com>
2021-01-23 20:31:25 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -7,9 +7,8 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <ReviewInput
-          addReview={this.props.addReview}
-          restaurantId={this.props.restaurant.id}
+        <ReviewInput restaurantId={this.props.restaurant.id} addReview={this.props.addReview}
+         
         />
         <Reviews
           reviews={this.props.reviews}
