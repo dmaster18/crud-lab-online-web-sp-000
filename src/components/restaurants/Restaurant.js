f831cab1407bd757f5a57ca8120a3e6ceafc1bcf
David Strauch <dmaster18@gmail.com>
2021-01-23 20:35:12 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -15,7 +15,7 @@ class Restaurant extends Component {
         <li>
           {this.props.text}
           <button onClick={() => this.handleOnClick()}> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
+          <ReviewsContainer restaurant={this.props}/>
         </li>
       </div>
     );
